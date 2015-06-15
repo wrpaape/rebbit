5eac1c305b5e4d6025eabb5dabcc580472c51c2a
Reid Paape <wrpaape@gmail.com>
2015-06-15 18:21:25 -0500

    unbreaking things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,18 @@ class UsersController < ApplicationController
 
 
   def create
-    if User.find_by(name: params[:name]) && User.find_by(email: params[:email])
-      redirect_to :back, alert: "username and email is already taken"
-    elsif User.find_by(name: params[:name])
-      redirect_to :back, alert: "username is already taken"
-    elsif User.find_by(email: params[:email])
-      redirect_to :back, alert: "email is already taken"
+    if User.find_by(name: user_params[:name]) && User.find_by(email: user_params[:email])
+      flash[:alert] = "username and email is already taken"
+      render :new
+    elsif User.find_by(name: user_params[:name])
+      flash[:alert] = "username is already taken"
+      render :new
+    elsif User.find_by(email: user_params[:email])
+      flash[:alert] = "email is already taken"
+      render :new
+    elsif user_params[:password] != user_params[:password_confirmation]
+      flash[:alert] = "passwords do not match"
+      render :new
     else
       user = User.new(user_params)
       if user.save
